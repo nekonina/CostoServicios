758cdff44ec9bf3991b0939b2063cfda13ce95b0
alejodiazg <09-10234@usb.ve>
2017-01-23 19:16:52 -0400

    agregadas funciones para calcular costo total

--- src/calcularTiempo.py
+++ src/calcularTiempo.py
@@ -2,34 +2,86 @@
 Created on 23 ene. 2017
 
 @author: iri_m_000
+@author: alejodiazg
 '''
 from datetime import*
 import sys
 
+MIN_MINUTES = 15
+MIN_SECONDS = 60 * MIN_MINUTES
+
 def introDatos()-> (date, date):
     formato = "%d/%m/%Y %H:%M:%S"
 
-    while True:
-        fecha_desde = input("fecha(dd/mm/aaaa hh:mm:ss) de inicio del servicio: ")
-        fecha_hasta = input("fecha (dd/mm/aaaa) de final de servicio")            
-        fecha_desde = datetime.strptime(fecha_desde, formato)
-        fecha_hasta = datetime.strptime(fecha_hasta, formato)
-        try:
-            assert(fecha_hasta.day-fecha_desde.day <= 7 and fecha_hasta.day-fecha_desde.day >= 0 )
-            break
-        
-        except:
-            print('el tiempo maximo para un servicio es de 7 dias')
-            
-        
+    fecha_desde = input("fecha(dd/mm/aaaa hh:mm:ss) de inicio del servicio: ")
+    fecha_hasta = input("fecha (dd/mm/aaaa) de final de servicio")            
+    fecha_desde = datetime.strptime(fecha_desde, formato)
+    fecha_hasta = datetime.strptime(fecha_hasta, formato)      
     return(fecha_desde,fecha_hasta)
- 
 
+
+#valida la diferencia entre dos fechas
+def validarDiferencia(inicio , fin) -> bool:
+    dif = fin - inicio
+    if(dif.days < 0 or dif.days > 7):
+        return False
+    #Hago esta verificacion por que los dias se redondean
+    #creo que es redundante
+    if(dif.seconds > 7 * 24 * 60 * 60 or dif.seconds < 0):
+        #print('Diferencia de segundos no valida')
+        return False
+    return True
+
+#funcion para validad si se cumplen los minutos minimos
+def validarMinutosMinimos(inicio , fin) -> bool:
+    dif = fin - inicio
+    if (dif.seconds < MIN_SECONDS):
+        return False
+    return True
+
+#oobtine el proximo dia
+def obtenerManana(fecha) -> date:
+    manana = fecha + datetime.timedelta(1)
+    return manana
+
+#calcula el costo de las horas trabajadas
+def calcularCosto(tarifa , horas) -> float:
+    return tarifa * horas
+
+#obtiene la tarifa segun el dia de la semana
+def obtenerTarifaDia(tarifas , diasemana) -> float:
+    #segun el dia de la semana obtener la tarifa
+    return 0.1
+
+#calcula las tarifas de las horas trabajadas cada dia
+def calcularParticionado(inicio , fin , tarifas) -> float:
+    suma = 0.0
+    dia = datetime.weekday(inicio)
+    tarifa = obtenerTarifaDia(tarifas, dia)
+    if(inicio.day != fin.day or inicio.month != fin.month or inicio.year != fin.year):
+        siguiente = obtenerManana(inicio)
+        #horas = diferencia que hizo iri
+        #suma = calcularCosto(tarifa , horas) + calcularParticionado(siguiente , fin)
+    else:
+        suma = 0.1
+        #horas = diferencia que hizo iri
+        #suma - calcularCosto(tarifa, horas)
+    return suma
+ 
 if __name__ == '__main__':
     
-    (fecha_desde, fecha_hasta) = introDatos()
-    diaI = datetime.weekday(fecha_desde)
-    diaF = datetime.weekday(fecha_hasta)
+    while True:
+        (fecha_desde, fecha_hasta) = introDatos()
+        diaI = datetime.weekday(fecha_desde)
+        diaF = datetime.weekday(fecha_hasta)
+        
+        if(validarDiferencia(fecha_desde, fecha_hasta)):
+            print('las fecha son validas')
+        
+        if(validarMinutosMinimos(fecha_desde, fecha_hasta)):
+            print('la cantidad de minutos minimos es valida')
+        else:
+            print('la cantidad de minutos minimos no es valida')
     
-    print("fecha1: ", diaI)
-    print("fecha1: ", diaF)
+        print("fecha1: ", diaI)
+        print("fecha1: ", diaF)
